0317f08e88c456ad9f511c7b84c03f8cfb81db36
Zach Mulder <zjmulder@berkeley.edu>
2013-09-17 16:11:14 -0700

    Finished Button Counter
    Finished button counter. Passes an int[] with int[0] being x coordiante
    and int[1] being y coordiante

--- milestoneThree.java
+++ milestoneThree.java
@@ -8,9 +8,9 @@ import lejos.nxt.SensorPort;
 import lejos.robotics.navigation.DifferentialPilot;
 
 public class milestoneThree {
-	
-	private int xLocation=0;
-	private int yLocation=0;
+
+	private static int xLocation=3;
+	private static int yLocation=4;
 
 	/**
 	 * @param args
@@ -28,27 +28,105 @@ public class milestoneThree {
 		int x = 0,y = 0;
 		getCoordinates(myrobot);
 		moveToCoordinates(myrobot,x,y);
-		
-	}
-	
 
-	private static void getCoordinates(Tracker myrobot) {
+	}
+
+
+	private static int[] getCoordinates(Tracker myrobot) {
 		//Validation - make sure it's in the boundaries.
 		//Just do it.
 		//1 is orange
 		//2 is left
 		//4 is right
-		//8 is back   
-		System.out.println("Enter coordinates");
-		System.out.println("Use arrows to navigate, enter and exit to increment");
-		
+		//8 is back 
+		//Bottom right corner is (99,63)
+		//Font height is 8, width 5
+		//6 x 8 grid
+		int xselected = 1;
+		boolean save = false;
+		int button;
+		int xgoto = xLocation;
+		int ygoto = yLocation;
+		LCD.drawString("Coordinates", 3, 0);
+		LCD.drawChar('X', 3, 3);
+		LCD.drawChar('Y', 13, 3);
+		LCD.drawInt(xLocation,3,4);
+		LCD.drawInt(yLocation,13,4);
+		LCD.drawString("Save:", 6,6);
+		LCD.drawString("Enter + L or R",1,7);
+		while (!save){
+			if (xselected==1) {
+				//Draw x triangle
+				for (int j=1;j<=3;j++){
+					for (int i=21-j;i<=19+j;i++){
+						LCD.setPixel(i,39+j, 1);}
+				}
+				//Undraw y triangle
+				for (int j=1;j<=3;j++){
+					for (int i=81-j;i<=79+j;i++){
+						LCD.setPixel(i,39+j, 0);}
+				}
+			}
+			else {
+				//Draw y triangle
+				for (int j=1;j<=3;j++){
+					for (int i=81-j;i<=79+j;i++){
+						LCD.setPixel(i,39+j, 1);}
+				}
+				//Undraw x triangle
+				for (int j=1;j<=3;j++){
+					for (int i=21-j;i<=19+j;i++){
+						LCD.setPixel(i,39+j, 0);}
+				}
+			}
+			button = Button.waitForAnyPress();
+			if (button==2 && xselected==1){}
+			else if(button==2){
+				xselected=1;
+			}
+			else if(button==4&&xselected==0){}
+			else if(button==4){
+				xselected=0;
+			}
+			else if(button==1){
+				if(xgoto==6&&ygoto==8){}
+				else if(xgoto==6){
+					ygoto=ygoto + (1-xselected)*1;
+				}
+				else if(ygoto==8){
+					xgoto=xgoto + xselected*1;
+				}
+				else{
+					xgoto=xgoto + xselected*1;
+					ygoto=ygoto + (1-xselected)*1;}
+			}
+			else if(button==8){
+				if(xgoto==0&&ygoto==0){}
+				else if(xgoto==0){
+					ygoto=ygoto - (1-xselected)*1;
+				}
+				else if(ygoto==0){
+					xgoto=xgoto - xselected*1;
+				}
+				else{
+					xgoto=xgoto - xselected*1;
+					ygoto=ygoto - (1-xselected)*1;}
+			}
+			else {
+				save=true;
+			}
+			LCD.drawInt(xgoto,3,4);
+			LCD.drawInt(ygoto,13,4);
+		}
+		LCD.clear();
+		System.out.println("Points saved!");
 		Button.waitForAnyPress();
-		
+		return new int[] {xgoto,ygoto};
 	}
 
 	private static void moveToCoordinates(Tracker myrobot, int x, int y) {
 		// 
-		
+
 	}
 
 
